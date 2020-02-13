33cd65cda7a53486a70bb457cd32fb53f354136a
Raywcl <2728928100@qq.com>
2020-02-13 17:32:07 +0800

    1.结构目录 2.entity完成： 商品、店铺等10个类的创建 3.entity对应数据库：建表语句：o2odb.sql

---
+++ src/main/java/com/java/o2o/entitiy/PersonInfo.java
@@ -0,0 +1,98 @@
+package com.java.o2o.entitiy;
+
+import java.util.Date;
+
+public class PersonInfo {
+
+	private Long userId;//id
+	private String name;//姓名
+	private String gender;//性别
+	private String phone;//电话
+	private String email;
+	private String profileImg;//头像
+	private Integer enableStatus;
+	//1.顾客，2.店家，3.超级管理员
+	private Integer userType;
+	private Date createTime;
+	private Date lastEditTime;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getProfileImg() {
+		return profileImg;
+	}
+
+	public void setProfileImg(String profileImg) {
+		this.profileImg = profileImg;
+	}
+
+	public Integer getEnableStatus() {
+		return enableStatus;
+	}
+
+	public void setEnableStatus(Integer enableStatus) {
+		this.enableStatus = enableStatus;
+	}
+
+	public Integer getUserType() {
+		return userType;
+	}
+
+	public void setUserType(Integer userType) {
+		this.userType = userType;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getLastEditTime() {
+		return lastEditTime;
+	}
+
+	public void setLastEditTime(Date lastEditTime) {
+		this.lastEditTime = lastEditTime;
+	}
+}
